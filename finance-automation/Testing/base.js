734f80566172e0a9ee0f4bd1cbb3b2c14adbc731
Mohit Rana <mohitrana67@gmail.com>
2020-05-02 22:43:01 -0400

    Creating a random box with random numbers and space to put in numbers to complete the sudoku

--- finance-automation/Testing/base.js
+++ finance-automation/Testing/base.js
@@ -1,19 +1,11 @@
 var createSudokuButton = document.getElementById("createSudoku");
 
-createSudokuButton.addEventListener("click",createSudoku);
+// createSudokuButton.addEventListener("click",createSudoku);
+
+document.onload = createSudoku();
 
 function createSudoku(){
     console.log("So you want to create a sudoku");
-    // let's create a sudoku
-    // var table = document.getElementById("sudokuTable");
-
-    // var row = table.insertRow(0);
-
-    // var cell1 = row.insertCell(0);
-    // var cell2 = row.insertCell(1);
-
-    // cell1.innerHTML = "New Cell 1"
-    // cell2.innerHTML = "New Cell 2"
     var tableRef = document.getElementById('sudokuTable').getElementsByTagName('tbody')[0];
     var i;
     for(i = 0; i <9; i++){
@@ -22,27 +14,28 @@ function createSudoku(){
         for(j=0;j<9;j++){
             var newCell = newRow.insertCell(j);
 
-            // creating a new text node]
             var e = i+","+j
-            var newText  = document.createTextNode(e);
 
-            // Creating a element INPUT
-            var x = document.createElement("INPUT");
+            var randomNumber = Math.floor(Math.random()*10);
 
-            // Settting attributes of the element INPUT
-            x.setAttribute("type", "text");
-            x.setAttribute("placeholder", "Pleace enter a number")
+            var randomBoxSelector = Math.floor((Math.random()*10))
 
-            //Appending a input type or text to the cell
-            newCell.appendChild(newText);
-            // newCell.appendChild(x);
+            numberToPutIn = 0
+            if(randomBoxSelector*randomNumber>=9){
+                numberToPutIn = randomNumber
+                var newText  = document.createTextNode(numberToPutIn);
+
+                newCell.appendChild(newText);   
+            }else{
+                var x = document.createElement("INPUT");
+
+                x.setAttribute("type", "text");
+                x.setAttribute("placeholder", "")
+
+                newCell.appendChild(x);
+            }
             newCell.classList.add("numberCell")
         }
-        // var newCell = newRow.insertCell(0);
-
-        // // Append a text node to the cell
-        // var newText  = document.createTextNode('New row');
-        // newCell.appendChild(newText);
     }   
 
     createSudokuButton.classList.add('disappear')
