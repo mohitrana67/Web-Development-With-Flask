59590b95de6adcdf740535212a05fb6d58664dfe
Mohit Rana <mohitrana67@gmail.com>
2020-05-03 21:22:45 -0400

    Adding static files(Javascript and CSS)

--- finance-automation/Testing/base.js
+++ finance-automation/Testing/base.js
@@ -1,43 +1,82 @@
 var createSudokuButton = document.getElementById("createSudoku");
 
-// createSudokuButton.addEventListener("click",createSudoku);
+createSudokuButton.addEventListener("click",solveSudoku);
 
-document.onload = createSudoku();
+// document.onload = createSudoku();
 
-function createSudoku(){
-    console.log("So you want to create a sudoku");
-    var tableRef = document.getElementById('sudokuTable').getElementsByTagName('tbody')[0];
-    var i;
-    for(i = 0; i <9; i++){
-        var newRow = tableRef.insertRow();
-        var j;
-        for(j=0;j<9;j++){
-            var newCell = newRow.insertCell(j);
 
-            var e = i+","+j
+// function createSudoku(){
+//     console.log("So you want to create a sudoku");
+//     var tableRef = document.getElementById('sudokuTable').getElementsByTagName('tbody')[0];
+//     var i;
+//     for(i = 0; i <9; i++){
+//         var newRow = tableRef.insertRow();
+//         var j;
+//         for(j=0;j<9;j++){
+//             var newCell = newRow.insertCell(j);
 
-            var randomNumber = Math.floor(Math.random()*10);
+//             var e = i+","+j
 
-            var randomBoxSelector = Math.floor((Math.random()*10))
+//             var randomNumber = Math.floor(Math.random()*10);
 
-            numberToPutIn = 0
-            if(randomBoxSelector*randomNumber>=9){
+//             var randomBoxSelector = Math.floor((Math.random()*10))
+
+//             numberToPutIn = 0
+//             if(randomBoxSelector*randomNumber>=9){
+//                 numberToPutIn = randomNumber
+//                 var newText  = document.createTextNode(numberToPutIn);
+
+//                 newCell.appendChild(newText);   
+//             }else{
+//                 var x = document.createElement("INPUT");
+
+//                 x.setAttribute("type", "text");
+//                 x.setAttribute("placeholder", "")
+
+//                 newCell.appendChild(x);
+//             }
+//             newCell.classList.add("numberCell")
+//         }
+//     }   
+
+    // createSudokuButton.classList.add('disappear')
+
+    // solve the sudoku
+    function solveSudoku(){
+        console.log("So you want to create a sudoku");
+        var tableRef = document.getElementById('sudokuTable').getElementsByTagName('tbody')[0];
+        var i;
+        for(i = 0; i <9; i++){
+            var newRow = tableRef.insertRow();
+            var j;
+            for(j=0;j<9;j++){
+                var newCell = newRow.insertCell(j);
+
+                var e = i+","+j
+
+                var randomNumber = Math.floor(Math.random()*10);
+
+                var randomBoxSelector = Math.floor((Math.random()*10))
+
+                numberToPutIn = 0
+                // if(randomBoxSelector*randomNumber>=9){
+                //     numberToPutIn = randomNumber
+                //     var newText  = document.createTextNode(numberToPutIn);
+
+                //     newCell.appendChild(newText);   
+                // }else{
+                //     var x = document.createElement("INPUT");
+
+                //     x.setAttribute("type", "text");
+                //     x.setAttribute("placeholder", "")
+
+                //     newCell.appendChild(x);
+                // }
                 numberToPutIn = randomNumber
                 var newText  = document.createTextNode(numberToPutIn);
+                newCell.appendChild(newText);
 
-                newCell.appendChild(newText);   
-            }else{
-                var x = document.createElement("INPUT");
-
-                x.setAttribute("type", "text");
-                x.setAttribute("placeholder", "")
-
-                newCell.appendChild(x);
+                newCell.classList.add("numberCell")
             }
-            newCell.classList.add("numberCell")
         }
-    }   
-
-    createSudokuButton.classList.add('disappear')
-
-}
+    }
---
+++ finance-automation/v02/static/js/profile.js
@@ -0,0 +1,2 @@
+console.log("We will start our journewy with javascript here")
+
